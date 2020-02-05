912511fedad43ad09e2d492b579c0bbee472411a
Mohd Iqbal <iqbal.safian@gmail.com>
2020-02-05 16:21:06 +0800

    Create endpoints for all posts

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,9 +1,43 @@
-import { response } from "express";
+import fetch from 'node-fetch';
+import { response } from 'express';
 
 export function getMostComments(req, res) {
   fetch('https://jsonplaceholder.typicode.com/comments')
     .then(response => {
-      console.log(response.length)
-      return response.length;
+      if (response.status != 200) {
+        console.log('Issue')
+        return;
+      }
+
+      let maxNumber = 0;
+
+      for (let i = 1; i <= 100; i++) {
+        let basicFilter = data.filter(d => d.postId === i);
+        
+        if (maxNumber < basicFilter.length) {
+          maxNumber = basicFilter.length;
+        }
+      }
+
+      console.log(maxNumber);
+
+      maxNumber.json();
+    })
+    .catch((err)=>{
+      return;
+    })
+}
+
+export function getPostById(req, res) {
+  fetch('https://jsonplaceholder.typicode.com/posts/' + req.params.id)
+    .then(data=> {
+      res.send(data);
+    })
+}
+
+export function getAllPosts(req, res) {
+  fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(data => {
+      res.json(data);
     })
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,11 @@
-import { getMostComments } from '../controller';
+import { getMostComments, getPostById } from '../controller';
 
 const routes = (app) => {
   app.route('/most-comments')
     .get(getMostComments);
+  
+  app.route('/post/:id')
+    .get(getPostById);
 }
 
 export default routes;
